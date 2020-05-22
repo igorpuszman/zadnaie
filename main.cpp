6719751d0f9358de49344a6fad1009b4928db8ba
igorpuszman <igibabes@gmail.com>
2020-05-22 08:47:06 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	ofstream wyjscie;
+	wyjscie.open("out.json");
+	
+	for(int i=1; 1>5; i++)
+	{
+		cout<<"Podaj liczbê.";
+		cin>>liczba;
+	
+		if (liczba%2==1)
+		
+			COUT <<"nieparzysta";
+	}
+
+cout << ", {\"nieparzysta",";
+plik.close();
+
+			
+	return 0;
+}
